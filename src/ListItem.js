13d0adc1c517575f2555c5968c00b24e5e0971a9
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-03-01 18:30:00 -0500

    first

---
+++ src/ListItem.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+render() {
+  return(
+    <li>Make the list!</li>
+  )
+}
+
+export default ListItem
---
+++ src/MyList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1> LIst of things I need to stop procrastinating: </h1>
+        <ol>
+          <li>Make the liste</li>
+          <li></li>
+          <li></li>
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default MyList;
